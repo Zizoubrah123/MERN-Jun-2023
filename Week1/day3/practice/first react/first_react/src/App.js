3064b28a0dcabd756d167967ac0bf6435768d876
aziz <johndoe@example.com>
2023-06-21 13:42:35 +0200

    first jsx

---
+++ Week1/day3/practice/first react/first_react/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1> hello dojo!</h1>
+        <h2>to do things:</h2>
+        <ul>
+          <li>learn react</li>
+          <li>climb mt everts</li>
+          <li>run a marathon</li>
+          <li>feed the dogs</li>  
+        </ul>
+    </div>
+  );
+}
+
+
+export default App;
